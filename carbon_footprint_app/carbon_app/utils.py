aff4acb737b06996c22e423ed1b3d91bdb977605
Emily McSweeney <emily.mcsweeney@mymtu.ie>
2026-02-15 10:45:52 +0000

    Addition of calculated emission factors

--- carbon_footprint_app/carbon_app/utils.py
+++ carbon_footprint_app/carbon_app/utils.py
@@ -1,17 +1,14 @@
 EMISSION_FACTORS = {
-    "petrol_car": 0.192,
-    "diesel_car": 0.171,
-    "electric_car": 0.053,
-    # "hybrid_car": ____
-    "bus": 0.105,
-    "train": 0.041,
-    # "e-bike/scooter": ____
-    "bike": 0.0,
-    "walk": 0.0,
-    'scooter': 105 # electric scooter or e-bike
+    "petrol_car": 0.192,    # defined in views
+    "diesel_car": 0.171,    # defined in views
+    "electric_car": 0.053,    # defined in views
+    "bus": 0.101,    # per passenger
+    "train": 0.041,     # per passenger
+    "bike": 0.0, 
+    "walk": 0.0, 
+    'scooter': 0.022 # electric scooter or e-bike
 }
 
 def calculate_emissions(distance,days,mode):
     factor = EMISSION_FACTORS.get(mode, 0)
     return distance * days * factor * 2
-
